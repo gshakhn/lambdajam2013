f26c9d2e79003dc0bd1d1fe3e208469f4c039f0e
Dan McGee <dmcgee@backstopsolutions.com>
2013-07-09 16:24:16 -0500

    Make it faster! 55s -> 34s with these changes

--- jams/learning/scala/src/main/scala/KnnCalculation.scala
+++ jams/learning/scala/src/main/scala/KnnCalculation.scala
@@ -6,12 +6,12 @@
  * To change this template use File | Settings | File Templates.
  */
 
-import scala.math._
+import Ordering.Implicits._
 
 object KnnCalculation {
   private def euclideanDistance(x: Entry, y: Entry) = {
     val a = x.pixels.zip(y.pixels)
-    a.par.map { case (f, l) => pow(f - l, 2) }.sum
+    a.par.map { case (f, l) => (f - l) * (f - l) }.sum
   }
 
   private def nearestNeighbors(entry: Entry, sampleData: Seq[Entry], k: Int) : Seq[Int] = {
@@ -21,24 +21,18 @@ object KnnCalculation {
     val sampleDistances = sampleData.par.map {
       euclideanDistance(entry, _)
     }
-    val zipped: List[(Int, Double)] = sampleNumbers.zip(sampleDistances).toList
+    val zipped = sampleNumbers.zip(sampleDistances).toList
     top(k, zipped).map {case (number, _) => number}
   }
 
-  def top (n: Int, li: List [(Int, Double)]) : List[(Int, Double)] = {
-
-    def updateSofar (sofar: List [(Int, Double)], el: (Int, Double)) : List [(Int, Double)] = {
-      // println (el + " - " + sofar)
+  def top [E, F : Ordering] (n: Int, li: List [(E, F)]) : List[(E, F)] = {
+    def updateSofar (sofar: List [(E, F)], el: (E, F)) : List [(E, F)] = {
       if (el._2 < sofar.head._2)
-        (el :: sofar.tail).sortWith (_._2 > _._2)
+        (el :: sofar.tail).sortWith(_._2 > _._2)
       else sofar
     }
 
-    /* better readable:
-      val sofar = li.take (n).sortWith (_ > _)
-      val rest = li.drop (n)
-      (sofar /: rest) (updateSofar (_, _)) */
-    (li.take (n). sortWith (_._2 > _._2) /: li.drop (n)) (updateSofar (_, _))
+    (li.take(n).sortWith(_._2 > _._2) /: li.drop(n)) (updateSofar(_, _))
   }
 
   private def commonestNeighbor(neighbors: Seq[Int]) : Int = {
